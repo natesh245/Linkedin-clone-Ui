b5d17193e7f53313f729a4ca3b65706ae83aeba9
Natesh <natesh246@gmail.com>
2021-08-28 14:52:40 +0530

    added cureve button with border blue

--- src/Components/Button/CurveButton/CurveButton.js
+++ src/Components/Button/CurveButton/CurveButton.js
@@ -1,12 +1,12 @@
 import "./CurveButton.css";
 
 export default function CurveButton({ title, color, style, Icon, type }) {
+  let extraClass;
+  if (color === "blue") extraClass = "blue-btn";
+  else if (color === "blue-white") extraClass = "blue-white-btn";
+  else extraClass = "white-btn";
   return (
-    <button
-      className={`curve-button ${color === "blue" ? "blue-btn" : "white-btn"}`}
-      style={style}
-      type={type}
-    >
+    <button className={`curve-button ${extraClass}`} style={style} type={type}>
       {Icon && <Icon />}
       {title && title}
     </button>
